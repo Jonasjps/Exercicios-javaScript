c8fee62cb40e0e3ebf1901f30ce08a7a55629cef
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-15 00:07:41 -0300

    exercícios js

---
+++ exercicio-19/app.js
@@ -0,0 +1,13 @@
+/*
+Este exercício será um pouquinho diferente dos anteriores.
+
+Seu desafio é desenvolver uma versão do quiz que:
+
+- Aborda um tema diferente (não pode ser de filmes);
+- Tem um tema de cores diferente do que foi apresentado na aula;
+- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
+
+Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, busque fazer esse exercício sem rever partes da aula.
+
+É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
+*/
