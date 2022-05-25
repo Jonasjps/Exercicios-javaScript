ebf5cf490dd0157a65eb2ec856e787696e4199ee
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-25 07:43:10 -0300

    Praticando js puro

---
+++ praticandoAleatório/script.js
@@ -0,0 +1,11 @@
+// O operador lógico not (!)
+
+let UsuarioNãoLogado = false
+
+if (!UsuarioNãoLogado) {
+    console.log('você precisa fazer login para continuar!')
+}
+
+
+// console.log(!true)
+// console.log(!false)
