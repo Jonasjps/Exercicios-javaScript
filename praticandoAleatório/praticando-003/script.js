e0eb866280bc3ad8fde2d76067f9a69cc1d855d4
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-28 17:48:20 -0300

    Praticando exercícios js switch

--- praticandoAleatório/praticando-003/script.js
+++ praticandoAleatório/praticando-003/script.js
@@ -4,4 +4,9 @@ let scores = [50, 25, 0, 30, 100, 20, 10]
  
 for (let x = 0; x < scores.length; x++) {
      console.log(scores[x])
+
+     if (scores[x] === 0){
+
+         continue
+     }
 }
---
+++ praticandoAleatório/praticando-004/script.js
@@ -0,0 +1,39 @@
+// Switch statement
+
+const Nota = 'D'
+
+switch (Nota) {
+    case 'A':
+        console.log('Sua nota foi A')
+        break
+    case 'B':
+        console.log('Sua nota foi B')
+        break
+    case 'C':
+        console.log('Sua nota foi C')
+        break
+    case 'D':
+        console.log('Sua nota foi D')
+        break
+    case 'E':
+        console.log('Sua nota foi E')
+        break
+    default: {
+        console.log('[ERRO] Opção invalida!')
+    }    
+}   
+
+
+
+
+// if (Nota === 'A') {
+//     console.log('Sua nota foi A')
+// }else if (Nota === 'B') {
+//     console.log('Sua nota foi B')
+// }else if (Nota === 'C') {
+//      console.log('Sua nota foi C')
+// }else if (Nota === 'D') {
+//     console.log('Sua nota foi D')
+// }else {
+//     console.log('Sua nota foi E')
+// }
