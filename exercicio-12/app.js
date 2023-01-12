2fd12913500f2e4360eb838369dcb3c576347462
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-12 18:49:59 -0300

    Atualizando exercícios

--- exercicio-12/app.js
+++ exercicio-12/app.js
@@ -25,13 +25,6 @@ const best2019Movies = [
   { title: 'Varda por Agnès', directedBy: 'Agnès Varda' },
   { title: 'A Vida Invisível', directedBy: 'Karim Aïnouz' }
 ]
-let mensage = 'Segundo o site Omelete, os melhores filmes de 2019 são:'
-
-best2019Movies.forEach(value => {
-  mensage += `
-  - ${value.title}, dirigido por ${value.directedBy}`
-})
-console.log(mensage)
 /*
   02
 
@@ -64,24 +57,14 @@ const youtubeUser = {
       gitHub: 'https://github.com/Roger-Melo'
     },
     country: 'Brasil'
-  },
-  Mensagem () {
-    // console.log(`Vídeos recentes de ${this.name}:`)
-
-
-    this.videos.recentVideos.forEach(value => {
-      // console.log(value.title)
-    })
   }
+  
 }
-// console.log(youtubeUser.Mensagem())
 /*
   03
 
   - Exiba o valor do PI no console.
 */
-
-// console.log(Math.PI)
 
 /*
   04
@@ -90,9 +73,6 @@ const youtubeUser = {
     console.
 */
 
-const firstNumber = 8.3
-// console.log(Math.ceil(firstNumber))
-
 /*
   05
 
@@ -100,8 +80,6 @@ const firstNumber = 8.3
     forma padrão, e exiba-o no console.
 */
 
-const secondNumber = 3.5
-// console.log(Math.round(secondNumber))
 /*
   06
 
@@ -109,17 +87,12 @@ const secondNumber = 3.5
     console.
 */
 
-const thirdNumber = 8.9
-// console.log(Math.floor(thirdNumber))
-
 /*
   07
 
   - Exiba no console o número abaixo com a parte decimal removida.
 */
 
-const fourthNumber = 5.5
-// console.log(Math.trunc(fourthNumber))
 
 /*
   08
@@ -127,5 +100,3 @@ const fourthNumber = 5.5
   - A cada vez que o index.html for carregado, exiba no console um número  
     aleatório de 0 à 10, incluindo 0 e 10.
 */
-const randonNumber = Math.random()
-// console.log(Math.round(randonNumber * 10))
