831fd780dfe22787f095157f40aa08b3f0140cb4
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-16 12:23:34 -0300

    Atualizando Exercícios

---
+++ praticandoAleatório/parents/app.js
@@ -0,0 +1,9 @@
+const article = document.querySelector('article')
+
+// Array.from(article.children).forEach(Element => {
+//     Element.classList.add('article-element')
+// })
+
+const title = document.querySelector('h2')
+
+console.log(title.nextElementSibling)
