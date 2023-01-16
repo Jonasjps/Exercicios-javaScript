43bc33acfa423f54d0688e54d47e5bd4bf90adec
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-16 16:54:02 -0300

    Atualizando exercícios

---
+++ praticandoAleatório/EventosDeClique/app.js
@@ -0,0 +1,21 @@
+const ul = document.querySelector('ul')
+// ul.remove()
+
+
+const lis = document.querySelectorAll('li')
+
+lis.forEach(li => {
+    li.addEventListener('click', event => {
+        const clickedElement = event.target
+
+        clickedElement.remove()
+    })
+})
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    const li = document.createElement('li')
+    li.textContent = 'Novo item'
+    ul.append(li)
+})
