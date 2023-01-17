f429d6461f5bf661e997f59456462a419c4913c2
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-17 17:28:27 -0300

    Atualizando Exercícios Js

--- praticandoAleatório/EventosDeClique/app.js
+++ praticandoAleatório/EventosDeClique/app.js
@@ -1,21 +1,37 @@
 const ul = document.querySelector('ul')
-// ul.remove()
-
-
-const lis = document.querySelectorAll('li')
-
-lis.forEach(li => {
-    li.addEventListener('click', event => {
-        const clickedElement = event.target
-
-        clickedElement.remove()
-    })
-})
 
 const button = document.querySelector('button')
 
 button.addEventListener('click', () => {
     const li = document.createElement('li')
+
     li.textContent = 'Novo item'
-    ul.prepend(li)//ou o método APPEND()
+    ul.prepend(li)
 })
+
+ul.addEventListener('click', event => {
+    clickedElement = event.target
+
+    if(clickedElement.tagName === 'LI') {
+        clickedElement.remove()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// button.addEventListener('click', () => {
+//     const li = document.createElement('li')
+//     li.textContent = 'Novo item'
+//     ul.prepend(li)//ou o método APPEND()
+// })
