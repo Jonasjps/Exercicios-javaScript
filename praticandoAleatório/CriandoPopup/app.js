34ef50051ddda20197e3a14e36d44582215f1d9f
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-18 09:04:46 -0300

    Atualizando Aplicação web

--- praticandoAleatório/CriandoPopup/app.js
+++ praticandoAleatório/CriandoPopup/app.js
@@ -5,16 +5,13 @@ button.addEventListener('click', () => {
     popup.style.display = 'block'
 })
 
-
-
 popup.addEventListener('click', event => {
-    const classNameOfClickedElement = event.target.classList[0]
-    console.log(classNameOfClickedElement)
-    // const ClassNames = ['popup-close','popup-wrapper','popup-link']
-    // const showClosePopup =  ClassNames.some(className => 
-    //     className === classNameOfClickedElement)
+    const classClickedOfElement = event.target.classList[0]
+    const ClassNames = ['popup-close','popup-wrapper','popup-link']
+    const condiçãoDoPopup = ClassNames.some(className => 
+         className === classClickedOfElement) 
 
-    // if (showClosePopup) {
-    //     popup.style.display = 'none'
-    // }
+    if(condiçãoDoPopup){
+        popup.style.display = 'none'
+    }
 })
