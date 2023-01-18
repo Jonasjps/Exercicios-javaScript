8d7592afb2b2a90e1daa19e7b479c12adb6b7a0b
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-17 21:33:08 -0300

    Atualizando Popup

---
+++ praticandoAleatório/CriandoPopup/app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button')
+const popup = document.querySelector('.popup-wrapper')
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block'
+})
+
+
+
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0]
+    const ClassNames = ['popup-close','popup-wrapper','popup-link']
+    const showClosePopup =  ClassNames.some(className => 
+        className === classNameOfClickedElement)
+        
+    if (showClosePopup) {
+        popup.style.display = 'none'
+    }
+})
