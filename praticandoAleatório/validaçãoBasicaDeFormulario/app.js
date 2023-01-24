fa72a1efc16e1e6cf7c0cd22010bb1160dda4e00
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2023-01-24 08:20:13 -0300

    Atualizando exercícios Js

--- praticandoAleatório/validaçãoBasicaDeFormulario/app.js
+++ praticandoAleatório/validaçãoBasicaDeFormulario/app.js
@@ -1,26 +1,76 @@
 const form = document.querySelector('.signup-form')
-const p = document.querySelector('.feedback')
+const feedback = document.querySelector('.feedback')
+// const usernameInput = document.querySelector('#username')
+// const p = document.querySelector('.feedback')
 
 const testeUserName = username => /^[a-zA-Z]{6,12}$/.test(username)
 
 form.addEventListener('submit', event => {
     event.preventDefault()
-
+    
+    
     const isValidUsername = testeUserName(event.target.username.value)
 
     if(isValidUsername) {
-        p.textContent = 'username válido =)'
+        feedback.textContent = 'Username valido =)'
         return
     }
-    p.textContent = 'O username deve conter entre 6 e 12 caracteres e deve conter apenas letras.'
+   feedback.textContent = 'O username deve conter entre 6 e 12 caracteres e deve conter apenas letras.'  
+    
 })
 
 form.username.addEventListener('keyup', event => {
     const isValidUsername = testeUserName(event.target.value)
-    
-    if(isValidUsername) {
+
+    console.log(event)
+    if (isValidUsername) {
         form.username.setAttribute('class', 'success')
         return
     }
-    form.username.setAttribute('class','error')
+
+    form.username.setAttribute('class', 'error')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const isValidUsername = testeUserName(event.target.username.value)
+
+    // if(isValidUsername) {
+    //     p.textContent = 'username válido =)'
+    //     return
+    // }
+    // p.textContent = 'O username deve conter entre 6 e 12 caracteres e deve conter apenas letras.'
+
+
+
+
+
+
+// form.username.addEventListener('keyup', event => {
+//     const isValidUsername = testeUserName(event.target.value)
+    
+//     if(isValidUsername) {
+//         form.username.setAttribute('class', 'success')
+//         return
+//     }
+//     form.username.setAttribute('class','error')
+// })
