43ee859c67e852e7025c7006f1275d125fa25a29
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-02-15 07:46:30 -0300

    Atualizando Exercícios Js

---
+++ praticandoAleatório/Quiz-interativo/app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.quiz-form')
+
+const trueQuestions = ['D', 'D', 'D', 'D'] 
+
+form.addEventListener('submit', event => {
+   event.preventDefault()
+   let score = 0
+   const questions = [
+      form.inputQuestion1.value,
+      form.inputQuestion2.value,
+      form.inputQuestion3.value,
+      form.inputQuestion4.value,
+   ]
+
+   questions.forEach((question,index) => { 
+       if(question === trueQuestions[index]) {
+          score += 25
+      }
+   })
+   console.log(score)
+})
