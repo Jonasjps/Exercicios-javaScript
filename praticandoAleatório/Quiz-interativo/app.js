48a24812753707107782b6c84f1fd99c8bf30a6d
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-02-18 17:36:31 -0300

    Atualizando Exercícios Js

--- praticandoAleatório/Quiz-interativo/app.js
+++ praticandoAleatório/Quiz-interativo/app.js
@@ -5,9 +5,9 @@ const trueQuestions = ['D', 'D', 'D', 'D']
 
 form.addEventListener('submit', event => {
    event.preventDefault()
-
-   let score = 0
    
+   let score = 0
+
    const questions = [
       form.inputQuestion1.value,
       form.inputQuestion2.value,
@@ -15,26 +15,28 @@ form.addEventListener('submit', event => {
       form.inputQuestion4.value,
    ]
 
-   questions.forEach((question,index) => {
+   questions.forEach((question, index) => {
       if(question === trueQuestions[index]) {
          score += 25
       }
    })
+
    scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth'
+      
    })
 
    finalResult.classList.remove('d-none')
-   
-   let counter = 0
-   
+
+   let counter  = 0
+
    const timer = setInterval(() => {
-      if(counter === score) {
+      if( counter === score) {
          clearInterval(timer)
       }
-      
-      finalResult.querySelector('span').textContent = `${counter++}%`
+      finalResult.querySelector('span').textContent = `${counter++}`
    }, 10)
+
 })
