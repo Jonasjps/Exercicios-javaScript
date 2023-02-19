5842bc455f10ec45c5377f2b09d9a0323f6326f0
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-02-19 13:01:16 -0300

    Atualizando Exercícios Js

--- praticandoAleatório/Quiz-interativo/app.js
+++ praticandoAleatório/Quiz-interativo/app.js
@@ -4,36 +4,52 @@ const finalResult = document.querySelector('.result')
 
 const alternativasCorrect = ['D', 'D', 'D', 'D']
 
-form.addEventListener('submit', event => {
-   event.preventDefault() 
-   
-   let score = 0
+let score = 0
 
-   const alternativas = [
-      form.inputQuestion1.value,
-      form.inputQuestion2.value,
-      form.inputQuestion3.value,
-      form.inputQuestion4.value,
-   ]
+const getrespostUsers = () => {
+   const alternativas = []
+   alternativasCorrect.forEach((_,index) => {
+      const iterando = form[`inputQuestion${index + 1}`].value
+      alternativas.push(iterando)
+   })
+   return alternativas
+}
+setAttibuteRespostas = (alternativas) => {
    alternativas.forEach((alternativa,index) => {
-      if(alternativa === alternativasCorrect[index]) {
+      const testandoAlternativas = alternativa === alternativasCorrect[index]
+      if(testandoAlternativas) {
          score += 25
       }
    })
-   finalResult.classList.remove('d-none')
-
-   scrollTo(0,0)
-
+}
+const rolandoAtela = () => {
+   scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+   })
+}
+const animandoAtela = () => {
    let counter = 0 
 
    const timer = setInterval(() => {
-     if(counter === score) {
+      const comparantoPontuaçaõ = counter === score
+     if(comparantoPontuaçaõ) {
       clearInterval(timer)
      }
-      finalResult.querySelector('span').textContent = `${counter}%`
-      counter++
+      finalResult.querySelector('span').textContent = `${counter++}%`
 
    }, 10)
+}
+const grupFunc =   event => {
+   event.preventDefault() 
+  
+   const alternativas =  getrespostUsers()
+   setAttibuteRespostas(alternativas)
+   finalResult.classList.remove('d-none')
+   rolandoAtela()
+   animandoAtela()
 
-   
-})
+}
+
+form.addEventListener('submit',grupFunc)
