c2a6b061b85b1547943b5e40ca9856b70611fe8d
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-02-19 21:01:58 -0300

    Atualizando Exercícios Js

---
+++ praticandoAleatório/Quiz-interativo/app.js
@@ -0,0 +1,59 @@
+const form = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
+
+let score = 0
+
+const alternativasCorrect = ['D', 'D', 'D', 'D']
+
+const getUserAnswers = () => {
+   let userAnswers = []
+   alternativasCorrect.forEach((_,index) => {
+      const iterando = form[`inputQuestion${index + 1}`].value
+      userAnswers.push(iterando)
+   })
+   return userAnswers
+}
+
+const caulcularApontuação = (userAnswers) => {
+   userAnswers.forEach((userAnswer,index) => {
+      const testandoAlternativasCorrect = userAnswer === alternativasCorrect[index] 
+      if(testandoAlternativasCorrect) {
+         score += 25
+      }
+   })
+}
+
+const showPontuação = ( ) => {
+   scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+   })
+   finalResult.classList.remove('d-none')
+}
+
+const animandoPontuação = () => {
+   let counter = 0
+   
+   const timer = setInterval(() => {
+      const stopPontuação = counter === score 
+      if(stopPontuação) {
+         clearInterval(timer)
+      }
+      finalResult.querySelector('span').textContent = `${counter++}%`
+      
+   }, 10)
+}
+
+const grupDeFunc = event => {
+   event.preventDefault()
+   
+   const userAswers = getUserAnswers()
+
+   caulcularApontuação(userAswers)
+   showPontuação()
+   animandoPontuação()
+
+}
+
+form.addEventListener('submit', grupDeFunc)
