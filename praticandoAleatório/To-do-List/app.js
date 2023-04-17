a1fcfcc81ca01047559a4d1d0e529dda93f8929f
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-04-17 07:23:22 -0300

    Salvando exercícios

--- praticandoAleatório/To-do-List/app.js
+++ praticandoAleatório/To-do-List/app.js
@@ -1,1 +1,6 @@
+const formAddTodo = document.querySelector('.form-add-todo')
 
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(event.target.add.value)
+} )
