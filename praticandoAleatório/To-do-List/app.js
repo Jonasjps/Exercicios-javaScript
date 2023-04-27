dacc42dd9f9c10422a1cbb339eee3afdc7857ee7
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-04-27 15:47:01 -0300

    Salvando Exercício

--- praticandoAleatório/To-do-List/app.js
+++ praticandoAleatório/To-do-List/app.js
@@ -4,9 +4,14 @@ const todosContainer = document.querySelector('.todos-container')
 formTodo.addEventListener('submit', event => {
     event.preventDefault()
     const valueDoInput = event.target.add.value.trim()
-    todosContainer.innerHTML += ` 
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${valueDoInput}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>`
+
+    if(valueDoInput.length) {
+        todosContainer.innerHTML += ` 
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${valueDoInput}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>`
+
+    }
+    event.target.reset()
 })
