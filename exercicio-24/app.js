0ee988ca457fed04b74e0a6c227579786fcc128a
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-04-28 11:52:27 -0300

    Salvando

--- exercicio-24/app.js
+++ exercicio-24/app.js
@@ -17,7 +17,7 @@ console.log(message.includes(':'))
 */
 
 const numbers = [979, 2673, 41, 77, 276, 554, 399, 385, 65, 726, 635, 833, 462]
-
+console.log(numbers.includes(635))
 /*
   A partir daqui, vamos treinar nossas skills de refatoração.
 
