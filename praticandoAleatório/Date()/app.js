cd5aebb720f7483da928a72d63aa5cef2a074b07
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-01 11:19:59 -0300

    Date()

---
+++ praticandoAleatório/Date()/app.js
@@ -0,0 +1,15 @@
+const present = new Date()
+
+console.log(present)
+console.log(present.getTime())
+console.log(present.getDay())
+console.log(present.getFullYear())
+console.log(present.getHours())
+console.log(present.getMilliseconds())
+console.log(present.getSeconds())
+console.log(present.getMonth())
+
+console.log(present.getMinutes())
+
+
+
