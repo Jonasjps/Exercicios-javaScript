b2fa0ded29fd75442f3d9c853b3c25f2e5912054
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-23 08:11:53 -0300

    Salvando Criação de Relogio Digital

--- praticandoAleatório/relogio-Digital/app.js
+++ praticandoAleatório/relogio-Digital/app.js
@@ -1,12 +1,22 @@
 const clockContainer = document.querySelector('.clock-container')
 
-const present = new Date()
+
 
 const clockSeconds = () => {
-    console.log(present)
-
+    const present = new Date()
+    const hours = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
+   
+    const clock = `
+    <span>${hours}</span>
+    <span>${minutes}</span>
+    <span>${seconds}</span>
+    `
+    
+    console.log(clock)
 }
 
 const time = setInterval(clockSeconds, 1000)
-clearInterval(time)
+// clearInterval(time)
 
