2dec8fa93fe8e4ae98e886e6b1083b3f7b07aedc
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-24 07:37:02 -0300

    Criando Relogio difgital

--- praticandoAleatório/relogio-Digital/app.js
+++ praticandoAleatório/relogio-Digital/app.js
@@ -3,5 +3,11 @@ const clockContainer = document.querySelector('.clock-container')
 
 const clock = () => {
    const present = new Date()
-   console.log(present)
+   const hours = present.getHours()
+   const minutes = present.getMinutes()
+   const seconds = present.getSeconds()
+   
+   console.log(hours, minutes, seconds)
 }
+
+setInterval(clock, 1000)
