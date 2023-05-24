5c62773e82ece908b6cbb068062d12c8ab3ac8f9
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-24 07:52:41 -0300

    Salvando Relogio digital Js

--- praticandoAleatório/relogio-Digital/app.js
+++ praticandoAleatório/relogio-Digital/app.js
@@ -6,8 +6,13 @@ const clock = () => {
    const hours = present.getHours()
    const minutes = present.getMinutes()
    const seconds = present.getSeconds()
-   
-   console.log(hours, minutes, seconds)
+
+   const clockHTML = `
+   <span>${String(hours).length === 1 ? `0${hours}`: hours}</span> :
+   <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
+   <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
+   `
+   clockContainer.innerHTML = clockHTML
 }
 
 setInterval(clock, 1000)
