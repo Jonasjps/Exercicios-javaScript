3567caae40c689469720227eae37847a1d272f03
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-26 07:53:26 -0300

    Salvando Js

--- praticandoAleatório/date-fns()/app.js
+++ praticandoAleatório/date-fns()/app.js
@@ -7,3 +7,8 @@ console.log(dateFns.format(present,'Do'))
 console.log(dateFns.format(present,'DD/MM/YYYY'))
 
 
+const past = new Date('may 26 2023 07:51:00')
+
+console.log(dateFns.distanceInWords(present,past))
+
+
