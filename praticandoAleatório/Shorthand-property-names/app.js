39ae1ccd087cf47f44ddc1ea0ff06be4f9cfad86
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-28 18:24:29 -0300

    Shorthand Property names Js

--- praticandoAleatório/Shorthand-property-names/app.js
+++ praticandoAleatório/Shorthand-property-names/app.js
@@ -1,14 +1,25 @@
 //Shorthand Property names
+const carro = 'carro'
+const onibus = 'onijbus'
+const moto = 'moto'
+const bicicleta = 'bicicleta'
 
-
-const cars = (carro, onibus, moto, bicicleta) => {
-    const MeusCars = {
-        carro,
-        onibus,
-        moto,
-        bicicleta
-    }
-    console.log(MeusCars)
+const MeusCars = {
+    carro,
+    onibus,
+    moto,
+    bicicleta
 }
 
-cars('carro', 'onibus', 'moto', 'bicicleta')
+console.log(MeusCars)
+// const cars = (carro, onibus, moto, bicicleta) => {
+//     const MeusCars = {
+//         carro,
+//         onibus,
+//         moto,
+//         bicicleta
+//     }
+//     console.log(MeusCars)
+// }
+
+// cars('carro', 'onibus', 'moto', 'bicicleta')
