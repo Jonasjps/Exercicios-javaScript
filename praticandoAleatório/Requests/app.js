ea0723a5724687adee51ef15d367aeab10e03745
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-06-08 13:34:12 -0300

    Criando Requests HTTP JS

--- praticandoAleatório/Requests/app.js
+++ praticandoAleatório/Requests/app.js
@@ -1,5 +1,12 @@
 const request = new XMLHttpRequest()
 
+request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4) {
+        console.log(request.response)
+    }
+    // console.log(request, request.readyState)
+})
+
+
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos' )
 request.send()
-console.log(request)
