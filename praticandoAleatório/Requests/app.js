a8ac104af9b5a23a4a5e5e3fa9e7577d47b965d2
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-06-26 06:51:59 -0300

    Atualizando

--- praticandoAleatório/Requests/app.js
+++ praticandoAleatório/Requests/app.js
@@ -1,11 +1,24 @@
-const request = new XMLHttpRequest()
+const getTodos = () => {
+    const request = new XMLHttpRequest()
+    
+    request.addEventListener('readystatechange', () => {
+        const isRequestOk = request.readyState === 4 && request.status === 200
+        const isRequetNotOK = request.readyState === 4
+        
+        if (isRequestOk) {
+            console.log(request, request.responseText)
+            return
+        }
+    
+        if(isRequetNotOK) {
+            console.log('Não foi possivel obter dados da Api')
+        }
+    })
+    
+    
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos' )
+    request.send()
 
-request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText)
-    }
-})
+}
 
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos' )
-request.send()
+getTodos()
