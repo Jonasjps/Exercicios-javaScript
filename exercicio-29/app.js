db05a9bbfc923b641025e9aac7a12a90021c87f4
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-13 13:56:28 -0300

    Atualizando Exercício Js

--- exercicio-29/app.js
+++ exercicio-29/app.js
@@ -12,48 +12,7 @@
     - Os requests devem ser sequenciais. Ou seja, um request só deve ser 
       executado quando o request anterior for finalizado.
 */
-const getPokemon = (url, callback) => {
-  const request = new XMLHttpRequest()
 
-  request.addEventListener('readystatechange', () => {
-    const isRequetOk = request.readyState === 4 && request.status === 200
-    const isRequetNotOK = request.readyState === 4
-         
-    if(isRequetOk) {
-      const data  = JSON.parse(request.responseText)
-      callback(null, data)
-      return
-    }
-
-    if(isRequetNotOK) {
-      callback('Não foi possível obter o Polémon ', null)
-    }
-
-  })
-  request.open('GET', url)
-  request.send() 
-}
-
-const pokemon = (error, data) => {
-  if(error) {
-    console.log(error)
-    return
-  }  
-   console.log(`Pokémon obtido: ${data.name}`)
-}
-  const idPokemon = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
-  const bulbasaur = idPokemon(1)
-  const charmander = idPokemon(4)
-  const squirtle = idPokemon(7)
-
-getPokemon(bulbasaur, (error, data) => {
-  pokemon(error, data)
-  getPokemon(charmander, (error, data) => {
-    pokemon(error, data)
-  getPokemon(squirtle, pokemon)  
-
-})
-})
 /*
   02
 
