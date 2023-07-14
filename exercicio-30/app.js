1662f5bd1d81dd174e263b226a5c4617245dcdb1
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-14 13:52:45 -0300

    Atualizando Exercício Js 30.1

--- exercicio-30/app.js
+++ exercicio-30/app.js
@@ -1,11 +1,10 @@
 /*
-  01
-
-  - Usando promises, faça um request para este endpoint:
-    https://jsonplaceholder.typicode.com/users
-  - Se o request estiver ok, exiba os objetos no console;
-  - Se o request não estiver ok, exiba no console "Não foi possível obter os 
-    dados dos usuários."
+01
+- Usando promises, faça um request para este endpoint:
+  https://jsonplaceholder.typicode.com/users
+- Se o request estiver ok, exiba os objetos no console;
+- Se o request não estiver ok, exiba no console "Não foi possível obter os 
+  dados dos usuários."
 */
 
 /*
