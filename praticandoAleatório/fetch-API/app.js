2df940bf0e60371623937acbfd19ba4d55db1e03
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-16 18:21:14 -0300

    Atualizando

--- praticandoAleatório/fetch-API/app.js
+++ praticandoAleatório/fetch-API/app.js
@@ -1,15 +1,8 @@
-const getUsers = async () => {
-    const response = await fetch('https://jsonPlaceholder.typicode.com/users')
-    const users = await response.json()
-    return users
-}
-
-console.log(getUsers())
-
-// fetch('https://jsonPlaceholder.typicode.com/users')
-//     .then(response => {
-//         console.log('response',response)
-//         return response.json()
-//     })
-//     .then(user => console.log(user))
-//     .catch(erro => console.log(erro))
+//  Encadeamento de Promises
+fetch('https://jsonPlaceholder.typicode.com/users')
+    .then(response => {
+        console.log('response',response)
+        return response.json()
+    })
+    .then(user => console.log(user))
+    .catch(erro => console.log(erro))
