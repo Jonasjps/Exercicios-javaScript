3c808889184362e4f7f7c5454b9d176124c78d90
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-22 12:17:08 -0300

    Atualizando Exercício Js 32

--- exercicio-32/app.js
+++ exercicio-32/app.js
@@ -22,3 +22,23 @@
 
 */
 
+const form = document.querySelector('form') 
+
+form.addEventListener('submit', async event => {
+  event.preventDefault()
+  const inputValue = event.target.search.value
+  const APIkey = 'QR0wHsfz9dl9pdBhaKHu52LzzTHyiH7R'
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIkey}&limit=1&q=${inputValue}`
+
+  try {
+    const response = await fetch(url)
+    const GIFsAPI = await response.json()
+
+    if(!response.ok) {
+      throw new Error('Não foi possível obter dados da API.')
+    }
+    console.log(GIFsAPI)
+  } catch (error) {
+    alert(`Erro: ${error.message}`)
+  }
+})
