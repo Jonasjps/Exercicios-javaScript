0c3cb609fa6624d1cd40f380db1b617a939342c6
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-27 22:59:19 -0300

    Atualizando Aplpication  weather Js

--- praticandoAleatório/Weather-Application/js/app.js
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -5,12 +5,15 @@ const cityTemperatureContainer =  document
     .querySelector('[data-js="city-temperature"]')
 const cityCard = document.querySelector('[data-js="city-card"]')
 let timeImg = document.querySelector('[data-js="time"]')
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
 cityForm.addEventListener('submit', async event => {
     event.preventDefault()
     const inputValue = event.target.city.value
     const [{Key, LocalizedName}] = await getCityData(inputValue)
-    const [{WeatherText, Temperature, IsDayTime}] = await getCityWeather(Key)
+    const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await
+         getCityWeather(Key)
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
 
     if(cityCard.classList.contains('d-none')) {
         cityCard.classList.remove('d-none')
@@ -22,6 +25,7 @@ cityForm.addEventListener('submit', async event => {
         timeImg.src = './src/night.svg'
     }
     
+    timeIconContainer.innerHTML = timeIcon
     cityNameContainer.textContent = LocalizedName
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
