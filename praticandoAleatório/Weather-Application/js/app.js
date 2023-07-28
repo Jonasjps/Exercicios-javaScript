fedb1c1a1e3ad4661c6949f768473f3211b6d3ce
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-27 21:28:01 -0300

    Atualizando Application Weather

---
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -0,0 +1,11 @@
+const cityForm = document.querySelector('[data-js="change-location"]')
+
+cityForm.addEventListener('submit', async event => {
+    event.preventDefault()
+    const inputValue = event.target.city.value
+    const [{Key, LocalizedName}] = await getCityData(inputValue)
+    const [{WeatherText, Temperature}] = await getCityWeather(Key)
+
+    console.log(WeatherText, Temperature.Metric.Value)
+    cityForm.reset()
+})
--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -4,8 +4,8 @@ const baseUrl = 'http://dataservice.accuweather.com/'
 const getCityUrl = CityName =>
     `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${CityName}`
 
-const getWeatherUrl = ({Key}) =>
-    `${baseUrl}currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-Br`  
+const getWeatherUrl = cityKey =>
+    `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-Br`  
 
 const fetchData = async url => {
     try {
@@ -22,8 +22,5 @@ const fetchData = async url => {
 }
 
 const getCityData = cityName => fetchData(getCityUrl(cityName)) 
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey))    
 
-const getCityWeather = async cityName => {
-    const [cityData] = await getCityData(cityName)
-    return fetchData(getWeatherUrl(cityData))    
-}
