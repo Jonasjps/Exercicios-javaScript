a6a83dbfa7d73979cd80f0427ca877a326399092
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-29 10:00:40 -0300

    projeto Weather Js puro

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -1,9 +1,23 @@
 const APIKey = 'PyeBNPXTFvCPegzzt4xQ0hrTsa7THJbs'
-const cityUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=Recife`
+const cityUrl = cityName =>
+   `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName
+}`
 
-const getCityUrl = async () => {
-  const response = await fetch(cityUrl)
-    console.log(await response.json())
+const getCityUrl = async cityName => {
+
+  try {
+    const response = await fetch(cityUrl(cityName))
+
+    if(!response.ok) {
+      throw new Error('Não foi possível obter dados da API')
+    }
+
+    const [cityData] = await response.json()
+    console.log(cityData)
+    
+  } catch ({name, message})  {
+    alert(`${name}: ${message}`)
+  }
 }
 
-getCityUrl()
+getCityUrl('Brasília')
