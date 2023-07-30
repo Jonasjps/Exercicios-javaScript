03b1735ddb0048b848bef99d48afeaa6da52f6b4
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-30 17:16:28 -0300

    Atualizando Application Weather Js puro

--- praticandoAleatório/Weather-Application/js/app.js
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -3,14 +3,17 @@ const cityData = document.querySelector('[data-js="city-card"]')
 const cityNameContainers = document.querySelector('[data-js="city-name"]')
 const cityWeatherContainers = document.querySelector('[data-js="city-weather"]')
 const cityTemperatureContainers = document.querySelector('[data-js="city-temperature"]')
-const img = document.querySelector('[data-js="time"]')
+let img = document.querySelector('[data-js="time"]')
+const timeIcon = document.querySelector('[data-js="time-icon"]')
+
 
 cityForm.addEventListener('submit', async event => {
     event.preventDefault()
     const inputValue = event.target.city.value
     const [{Key, LocalizedName}] = await getCityData(inputValue)
-    const [{WeatherText, Temperature, IsDayTime}] = await getCityWeather(Key)
-
+    const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeather(Key)
+    const Icon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
+    
     if(cityData.classList.contains('d-none')) {
         cityData.classList.remove('d-none')
     }
@@ -21,7 +24,7 @@ cityForm.addEventListener('submit', async event => {
         img.src = `./src/night.svg`
     }
     
-
+    timeIcon.innerHTML = Icon
     cityNameContainers.textContent = LocalizedName
     cityWeatherContainers.textContent = WeatherText
     cityTemperatureContainers.textContent = Temperature.Metric.Value
