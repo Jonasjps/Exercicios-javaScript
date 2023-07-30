59a07586e7684d801d717c6cb9f03e216da6577f
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-30 16:34:46 -0300

    Atualizando Application

--- praticandoAleatório/Weather-Application/js/app.js
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -3,16 +3,24 @@ const cityData = document.querySelector('[data-js="city-card"]')
 const cityNameContainers = document.querySelector('[data-js="city-name"]')
 const cityWeatherContainers = document.querySelector('[data-js="city-weather"]')
 const cityTemperatureContainers = document.querySelector('[data-js="city-temperature"]')
+const img = document.querySelector('[data-js="time"]')
 
 cityForm.addEventListener('submit', async event => {
     event.preventDefault()
     const inputValue = event.target.city.value
     const [{Key, LocalizedName}] = await getCityData(inputValue)
-    const [{WeatherText, Temperature}] = await getCityWeather(Key)
-    
+    const [{WeatherText, Temperature, IsDayTime}] = await getCityWeather(Key)
+
     if(cityData.classList.contains('d-none')) {
         cityData.classList.remove('d-none')
     }
+
+    if(IsDayTime){
+        img.src = `./src/day.svg`
+    }else {
+        img.src = `./src/night.svg`
+    }
+    
 
     cityNameContainers.textContent = LocalizedName
     cityWeatherContainers.textContent = WeatherText
