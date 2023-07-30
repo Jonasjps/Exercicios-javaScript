b3b517b0cc35a7f1a6e8a9a9d0ba0aef6266b8af
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-07-29 22:43:55 -0300

    Atualizando Application Js

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -3,40 +3,39 @@ const APIkey = 'zH9PDMh6F4A2NhKoQ3cYezmmvAN8SoQU'
 const getCityUrl = cityName =>
    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIkey}&q=${cityName}`
 
-const getCityData = async cityName => {
+const getData = async url => {
   try {
-    const cityUrl = getCityUrl(cityName)
-    const response = await fetch(cityUrl)
+    const response = await fetch(url)
 
     if(!response.ok) {
       throw new Error('Não foi possível obter dados.')
     }
 
-    const [cityData] = await response.json()
-    return cityData
+    return await response.json()
+    
 
   } catch ({name, message}) {
     alert(`${name}: ${message}`)
   }
 }
 
-const getCityWeather = async cityName => {
-  try {
-    const {Key} = await getCityData(cityName)
-    const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIkey}&language=pt-br`
-    const response = await fetch(weatherUrl)
+// const getCityWeather = async cityName => {
+//   try {
+//     const {Key} = await getCityData(cityName)
+//     const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIkey}&language=pt-br`
+//     const response = await fetch(weatherUrl)
     
-    if(!response.ok) {
-      throw new Error('Não foi possível obter dados.')
-    }
+//     if(!response.ok) {
+//       throw new Error('Não foi possível obter dados.')
+//     }
 
-    const [weatherData] = await response.json()
-    return weatherData
+//     const [weatherData] = await response.json()
+//     return weatherData
 
-  } catch ({name, message}) {
-    alert(`${name}: ${message} `)
-  }
-}
+//   } catch ({name, message}) {
+//     alert(`${name}: ${message} `)
+//   }
+// }
 
-getCityWeather('Brasília')
-  .then(console.log)
+// getCityWeather('Brasília')
+//   .then(console.log)
