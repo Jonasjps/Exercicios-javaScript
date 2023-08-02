d4c8019cc715a9c7a5f178638fc6355df5b8d864
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-08-02 06:40:02 -0300

    Atualizando Application Weather Js puro

---
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -0,0 +1,38 @@
+const APIKey = 'G5C56jz2yBuF6ExHvWQnb8rQTldkPcU7'
+
+const getCityUrl = cityName =>
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+    try {
+        const cityUrl = getCityUrl(cityName)
+        const response = await fetch(cityUrl)
+       
+        if(!response.ok) {
+            throw new Error('Não foi possível obter dados.')
+        }
+
+        const [cityData] = await response.json()
+        return cityData
+    } catch ({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityWeather = async cityName => {
+    try {
+        const {Key} = await getCityData(cityName)       
+        const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+        const response = await fetch(weatherUrl)
+        if(!response.ok) {
+            throw new Error('Não foi possível obter dados API.')
+        }
+        const [weatherData] = await response.json()
+        return weatherData
+    } catch ({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityWeather('Brasília')
+    .then(console.log)
