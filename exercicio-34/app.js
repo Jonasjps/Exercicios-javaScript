325a5a10103d721a756351d7ba6e9ba9c9836888
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-08-04 07:54:50 -0300

    Atualizando

--- exercicio-34/app.js
+++ exercicio-34/app.js
@@ -16,8 +16,22 @@ const inverted = value => {
     .split('')
     .reverse()
     .join('')
-  
 }
+
+const reverseString = (str) => {
+  let newString = ''
+
+  for(let i = str.length -1; i >= 0; i-- ) {
+    newString += str[i]
+  }
+
+  return newString
+}
+let name = 'Maria'
+console.log(name.length -1s)
+
+console.log(reverseString('12345'))
+
 console.log(inverted('Jonas'))
 /*
   02
