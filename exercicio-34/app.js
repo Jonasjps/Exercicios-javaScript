0aa0b8c10f4b7685d1e6e39901bdd6daad2989fc
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-08-06 22:11:03 -0300

    Atualizando

--- exercicio-34/app.js
+++ exercicio-34/app.js
@@ -11,28 +11,9 @@
   - Após implementar a função, implemente outra versão da função. Essa 2ª 
     versão deve fazer o mesmo que a função anterior faz, mas de forma diferente.
 */
-const inverted = value => {
-  return value
-    .split('')
-    .reverse()
-    .join('')
-}
 
-const reverseString = (str) => {
-  let newString = ''
 
-  for(let i = str.length -1; i >= 0; i-- ) {
-    newString += str[i]
-  }
 
-  return newString
-}
-let name = 'Maria'
-name = name.length -1
-console.log(name.length -1)
-
-console.log(reverseString('12345'))
-console.log(inverted('Jonas'))
 /*
   02
   
@@ -42,11 +23,13 @@ console.log(inverted('Jonas'))
 const numbers = [5, 20, 7, 32, 47, 15, 83, 91, 27, 33]
 let foundNumber = false
 
-const checkingNumber = number => number === 15 
-? foundNumber = true 
-: foundNumber
+numbers.forEach(number => {
+  if (number === 15) {
+    foundNumber = true
+  }
+})
 
-numbers.forEach(checkingNumber)
+
 
 console.log(foundNumber)
 
