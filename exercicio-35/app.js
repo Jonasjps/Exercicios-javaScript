b4ec02a6ba66361f07a1cc0c4d9aa006a606270d
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-08-10 20:44:33 -0300

    Atualizando Exercícios

--- exercicio-35/app.js
+++ exercicio-35/app.js
@@ -4,11 +4,7 @@
   - Implemente uma função que recebe o nome da key de um item da localStorage 
     e retorna o valor da key parseado para objeto javascript.
 */
-const local = myName => {
-  return  myName = localStorage.key
-}
 
-console.log('Jonas')
 
 /*
   02
