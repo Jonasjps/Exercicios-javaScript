0544544c186cea0ec02a8e996e7cbd2fa732c18a
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-08-30 06:39:13 -0300

    Atualizando  Exercício 37 Js

--- exercicio-37/app.js
+++ exercicio-37/app.js
@@ -13,13 +13,14 @@ class Animal {
 
 class Rabbit extends Animal {
   constructor (name) {
-    this.name = name
+    super(name)
     this.created = new Date()
   }
 }
 
-// let rabbit = new Rabbit('White Rabbit')
+let rabbit = new Rabbit('White Rabbit')
 
+console.log(rabbit)
 /*
   02
 
