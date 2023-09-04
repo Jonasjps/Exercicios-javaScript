0199ff1885db04a6221fcc658a9952319fb38df4
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-09-03 22:38:44 -0300

    Atualizando

--- praticandoAleatório/testes-aleatorios/app.js
+++ praticandoAleatório/testes-aleatorios/app.js
@@ -1,20 +1,18 @@
 const clockContainer = document.querySelector('[data-js="clock"]')
 
-
 const setRelogioDigital = () => {
     const data = new Date()
     let hours = data.getHours()
     let minutes = data.getMinutes()
     let seconds = data.getSeconds()
-    
+
     const clock = ` 
-    <span>${hours}</span>
-    <span>${minutes}</span>
-    <span>${seconds}</span>
+    <span>${String(hours).length === 1 ? `0${hours}` : hours }</span>:
+    <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span>:
+    <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
     `
-    clockContainer.innerHTML = clock
+    return clockContainer.innerHTML = clock
 }
 
 setInterval(setRelogioDigital, 1000)
 
-console.log(setRelogioDigital())
