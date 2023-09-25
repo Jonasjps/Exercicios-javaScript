09ee289600cc8567ec534b451078230fb2c65e97
Jonasjps <jonaspsjw@hotmail.com>
2023-09-25 20:11:28 -0300

    Atualizando

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -4,22 +4,23 @@ const baseUrl =  `http://dataservice.accuweather.com`
 const cityUrl = cityName => 
     `${baseUrl}/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}&language=pt-br`
 
-const getCityWeather = async cityName => {
+const fetchData = async url => {
     try {
-        const url = cityUrl(cityName)
+
         const response = await fetch(url)
 
         if(!response.ok) {
             throw Error('Não foi possível obter dados da api.')
         }
 
-        const [cityData] =  await response.json()
-        return cityData
-
+        return await response.json()
+        
     } catch ({name, message}) {
         alert(`${name}: ${message} `)
     }
 }
+
+const getCityWeather = cityName => fetchData(cityUrl(cityName))
 
 // const getCityWeatherData = async cityName => {
 //     try {
