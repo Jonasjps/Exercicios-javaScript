f6d86480c0edfa9c9a0acb9d281d61cd22803bef
Jonasjps <jonaspsjw@hotmail.com>
2023-09-26 07:42:08 -0300

    Atualizando

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -3,6 +3,7 @@ const baseUrl =  `http://dataservice.accuweather.com`
 
 const cityUrl = cityName => 
     `${baseUrl}/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}&language=pt-br`
+const weatherUrl = cityKey => `${baseUrl}/currentconditions/v1/${cityKey.Key}?apikey=${apiKey}&q=${cityKey.LocalizedName}&language=pt-br`
 
 const fetchData = async url => {
     try {
@@ -23,9 +24,7 @@ const getCityWeather = cityName => fetchData(cityUrl(cityName))
 
 const getCityWeatherData = async cityName => {
     const [cityKey] = await getCityWeather(cityName)
-    const weatherUrl = `${baseUrl}/currentconditions/v1/${cityKey.Key}?apikey=${apiKey}&q=${cityName}&language=pt-br`
-    return await fetchData(weatherUrl)
-    
+    return await fetchData(weatherUrl(cityKey))
 }
 
 getCityWeatherData('São Paulo')
