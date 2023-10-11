c8d908bf7baf69856a98dc6c8fe894a5db2ee6a2
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-10-10 23:57:19 -0300

    Atualizando

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -1,5 +1,5 @@
 const APIKey = 'xt6BOTgFyMi3NEx6MKeVzabJluEXuAG6'
 
-const cityUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q='Brasília`
+const cityUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=Brasília`
 
  fetch(cityUrl)
