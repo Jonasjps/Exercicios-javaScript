6e1adf55fae48970968e5846fa2d7d4efd9fd9ec
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-10-12 20:06:11 -0300

    Atualizando Reafactoring Application

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -3,40 +3,41 @@ const APIKey = 'xt6BOTgFyMi3NEx6MKeVzabJluEXuAG6'
 const cityUrl = cityName => 
     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-const getCityData = async cityName => {
+const fetchData = async url => {
     try{
-        const url = cityUrl(cityName)
+
         const response = await fetch(url)
 
         if(!response.ok) {
             throw Error('Não foi possível obter dados da api.')
         }
 
-        const cityData = await response.json()
-        return cityData
+        return await response.json()
 
     }catch ({name, mesage}) {
         alert(`${name}: ${mesage}`)
     }
 }
 
-const getCityWeather = async cityName => {
-    try {
-        const [cityWeatherData] = await getCityData(cityName)
-        const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${cityWeatherData.Key}?apikey=${APIKey}&language=pt-br`
-        const response = await fetch(weatherUrl)
+const getCityData = cityName => fetchData(cityUrl(cityName))
 
-        if(!response.ok) {
-            throw Error('Não foi possível obter dados da api!')
-        }
+// const getCityWeather = async cityName => {
+//     try {
+//         const [cityWeatherData] = await getCityData(cityName)
+//         const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${cityWeatherData.Key}?apikey=${APIKey}&language=pt-br`
+//         const response = await fetch(weatherUrl)
 
-        const [weatherData] = await response.json()
-        return weatherData
+//         if(!response.ok) {
+//             throw Error('Não foi possível obter dados da api!')
+//         }
+
+//         const [weatherData] = await response.json()
+//         return weatherData
         
-    } catch ({name , mesage}) {
-        alert(`${name}: ${mesage}`)
-    }
-}
+//     } catch ({name , mesage}) {
+//         alert(`${name}: ${mesage}`)
+//     }
+// }
 
-getCityWeather('Brasília')
+getCityData('Brasília')
     .then(console.log)
