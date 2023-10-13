7faad8fee0c24760c5281b1fc41520fac0ecc016
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-10-12 21:50:55 -0300

    Atualizando Application Js

--- praticandoAleatório/Weather-Application/js/app.js
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -3,17 +3,26 @@ const cityNameContainers = document.querySelector('[data-js="cityName"]')
 const cityWeatherContainers = document.querySelector('[data-js="cityWeather"]')
 const cityTemperatureContainers = document.querySelector('[data-js="cityTemperature"]')
 const cityCard = document.querySelector('[data-js="cityCard"]')
+const cityImg = document.querySelector('[data-js="time"]')
+const timeIcon = document.querySelector('[data-js="time-icon"]')
 
 formInput.addEventListener('submit', async event => {
     event.preventDefault()
     const inputValue = event.target.city.value
     const [{Key, LocalizedName}] = await getCityData(inputValue)
     const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeatherData(Key)
+    const imgIcon = `<img src="./src/icons/${WeatherIcon}.svg">`
 
     if (cityCard.classList.contains('d-none')) {
         cityCard.classList.remove('d-none')
     }
-    
+
+    if(IsDayTime) {
+        cityImg.src ='./src/day.svg'
+    } else {
+        cityImg.src ='./src/night.svg'
+    }
+    timeIcon.innerHTML = imgIcon
     cityNameContainers.innerHTML = LocalizedName
     cityWeatherContainers.innerHTML = WeatherText
     cityTemperatureContainers.innerHTML = Temperature.Metric.Value
