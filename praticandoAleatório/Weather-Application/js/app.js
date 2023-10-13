29e41f7ebea7dd459a0ba968af5b7da69bca6737
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-10-12 21:23:34 -0300

    Atualizando Application Js

--- praticandoAleatório/Weather-Application/js/app.js
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -2,6 +2,7 @@ const formInput = document.querySelector('[data-js="change-location"]')
 const cityNameContainers = document.querySelector('[data-js="cityName"]')
 const cityWeatherContainers = document.querySelector('[data-js="cityWeather"]')
 const cityTemperatureContainers = document.querySelector('[data-js="cityTemperature"]')
+const cityCard = document.querySelector('[data-js="cityCard"]')
 
 formInput.addEventListener('submit', async event => {
     event.preventDefault()
@@ -9,7 +10,13 @@ formInput.addEventListener('submit', async event => {
     const [{Key, LocalizedName}] = await getCityData(inputValue)
     const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeatherData(Key)
 
+    if (cityCard.classList.contains('d-none')) {
+        cityCard.classList.remove('d-none')
+    }
+    
     cityNameContainers.innerHTML = LocalizedName
     cityWeatherContainers.innerHTML = WeatherText
     cityTemperatureContainers.innerHTML = Temperature.Metric.Value
+    
+    formInput.reset()
 })
