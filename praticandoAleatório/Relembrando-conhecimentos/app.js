0db4e69149d716303d01b9c1b49ed6e0c082f025
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-10-31 08:16:25 -0300

    Atualizando

--- praticandoAleatório/Relembrando-conhecimentos/app.js
+++ praticandoAleatório/Relembrando-conhecimentos/app.js
@@ -6,3 +6,14 @@ const numbers = [12, 13, 3, 1, 6]
 const result = numbers.some(item => item > 15)
 console.log(result)
 
+/**
+ MAP: O metódo map é um método de array onde ele cria um novo array, com a
+  mesma quantidade de itens porem com os itens alterados. veja o exemplo abaixo:
+ */
+
+
+  let names = {name: 'Jonas', lastName: 'Pessoa', gender: 'Masculino', age: 28}
+
+  const namesAlterados = names.map(item => item)
+
+  console.log(namesAlterados)
