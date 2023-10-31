796a0e5ccf2f926151cb2180f68d5a0c251ffb5e
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-10-30 22:31:10 -0300

    Atualizando

---
+++ praticandoAleatório/Relembrando-conhecimentos/app.js
@@ -0,0 +1,8 @@
+// O metódo some executa uma função de callback, retornando assim assim um
+// boolean indicando se a operação é true ou false. como no exemplo abaixo:
+
+const numbers = [12, 13, 3, 1, 6]
+
+const result = numbers.some(item => item > 15)
+console.log(result)
+
