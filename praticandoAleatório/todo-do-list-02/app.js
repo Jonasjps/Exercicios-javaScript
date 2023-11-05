74e0ae9ee7358534d43400a5997688a0fa2ade6d
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-05 12:57:42 -0300

    Atualizando

--- praticandoAleatório/todo-do-list-02/app.js
+++
@@ -1,6 +0,0 @@
-const formInput = document.querySelector('.form-add-todo')
-
-formInput.addEventListener('submit', event => {
-    event.preventDefault()
-    console.log(event.target.add.value)
-})
