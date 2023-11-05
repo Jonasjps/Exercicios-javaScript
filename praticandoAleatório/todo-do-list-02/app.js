aeb98b7e1c62ccbe8f497cd629c489779e815027
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-05 19:30:50 -0300

    Atualizando Atualizando

--- praticandoAleatório/todo-do-list-02/app.js
+++ praticandoAleatório/todo-do-list-02/app.js
@@ -7,15 +7,21 @@ formAddTodo.addEventListener('submit', event => {
     const inputValue = event.target.add.value.trim()
         
     if(inputValue.length) {
-            todosContainer.innerHTML += 
-            `
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <span>${inputValue}</span>
-                    <i class="far fa-trash-alt delete"></i>
-                </li>
-            `
-        }
+        todosContainer.innerHTML += 
+        `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${inputValue}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+        `
+    }
 
-
-     event.target.reset()
+    event.target.reset()
 } )
+
+todosContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+        if(Array.from(clickedElement.classList).includes('delete')) {
+            clickedElement.parentElement.remove()
+        }
+})
