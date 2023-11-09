c9597a3b78f03109ee49192b33a527e18867442e
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-09 20:45:30 -0300

    Atualizando app js

--- praticandoAleatório/todo-do-list-02/app.js
+++ praticandoAleatório/todo-do-list-02/app.js
@@ -15,6 +15,12 @@ formInput.addEventListener('submit', event => {
             `
 
     }
-
     event.target.reset()
+})
+
+todosContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+    if((Array.from(clickedElement.classList)).includes('delete')) {
+        clickedElement.parentElement.remove()
+    }
 })
