fa7e37699775d4deb14f66e4f89c2ac4a742cc16
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-09 13:06:19 -0300

    Atualizando app js

--- praticandoAleatório/todo-do-list-02/app.js
+++ praticandoAleatório/todo-do-list-02/app.js
@@ -3,22 +3,24 @@ const todosContainer = document.querySelector('.todos-container')
 
 formInput.addEventListener('submit', event => {
     event.preventDefault() 
-    const inputValue = event.target.add.value
-
-    todosContainer.innerHTML += 
-    `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputValue}</span>
-            <i class="far fa-trash-alt delete" ></i>
-        </li>
-    `
+    const inputValue = event.target.add.value.trim()
+    if(inputValue.length) { 
+        todosContainer.innerHTML += 
+        `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${inputValue}</span>
+                <i class="far fa-trash-alt delete" ></i>
+            </li>
+        `
+    }
     event.target.reset()
 })
 
 todosContainer.addEventListener('click', event => {
-    const inputValue = event.target
-    Array.from(todosContainer.children)
-    console.log(Array.from(todosContainer.children))
+    const clickedElement = event.target
+    if (Array.from(clickedElement.classList).includes('delete')) {
+        clickedElement.parentElement.remove()
+    }
 })
 
 
