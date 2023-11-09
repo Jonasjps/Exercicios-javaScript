76105dfa9259002726cbd40f433c6a22cad25349
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-09 19:14:54 -0300

    Atualizando App  js

---
+++ praticandoAleatório/todo-do-list-02/app.js
@@ -0,0 +1,20 @@
+const formInput = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+
+formInput.addEventListener('submit', event => {
+    event.preventDefault() 
+    const inputValue = event.target.add.value
+
+    if(inputValue.length) {
+        todosContainer.innerHTML += 
+            `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${inputValue}</span>
+                <i class="far fa-trash-alt delete" ></i>
+            </li>
+            `
+
+    }
+
+    event.target.reset()
+})
