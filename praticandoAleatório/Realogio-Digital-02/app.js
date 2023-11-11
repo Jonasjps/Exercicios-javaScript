fdf8359a37deb66e9c0c7789705dc70003722927
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-11 12:57:44 -0300

    Atualizando

--- praticandoAleatório/Realogio-Digital-02/app.js
+++ praticandoAleatório/Realogio-Digital-02/app.js
@@ -1,2 +1,17 @@
+const clockContainer = document.querySelector('.clock-container')
 
+const clock = () => {
+    const present = new Date()
+    const hours = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
 
+    const clockHTML =  `
+        <span>${hours}</span>
+        <span>${minutes}</span>
+        <span>${seconds}</span>
+    `
+    console.log(clockHTML)
+}
+
+setInterval(clock, 1000)
