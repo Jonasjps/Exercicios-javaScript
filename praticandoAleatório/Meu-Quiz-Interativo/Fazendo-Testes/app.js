05254dcc8b01530387f537b6effe85da71d21576
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-11-17 07:30:18 -0300

    Atualizando

--- praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
+++ praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.quiz-form')
 
+let alternativesCorrects = ['D', 'D', 'D', 'D']
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const getUserAnswers = alternativesCorrects.map((_, index) => {
+        return form[`inputQuestion${index + 1}`].value
+    }) 
+
+
+
+    console.log(getUserAnswers)
+    
+})
