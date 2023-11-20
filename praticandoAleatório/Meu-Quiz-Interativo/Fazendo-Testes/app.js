4427d24b6a92131303d23eaa2f764aad261439f1
Jonas Pessoa <Jonaspsjw@hotmail.com>
2023-11-19 21:19:10 -0300

    Atualizando

--- praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
+++ praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
@@ -1,15 +1,38 @@
 const form = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
 
 const correctAlternatives = ['D','D','D','D']
 
 form.addEventListener('submit', event => {
     event.preventDefault() 
     // obtendo resposta do usuario.
+    let score = 0
+
     const userAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
         form.inputQuestion3.value,
         form.inputQuestion4.value,
     ]
-    console.log(userAnswers)
+
+    //inserindo pontuação
+    userAnswers.forEach((userAnswer, index)=> {
+        if(userAnswer === correctAlternatives[index]) {
+            score += 25
+        }
+    })
+    
+    scrollTo(0,0)
+
+    finalResult.classList.remove('d-none')
+    
+    let counter = 0
+    
+    const time = setInterval(() => {
+        if(counter === score) {
+            clearInterval(time)
+        }
+        finalResult.querySelector('span').textContent = `${counter}%`
+        counter++
+    },10)
 })
