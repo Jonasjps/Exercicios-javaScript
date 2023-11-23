10f6df823ac1de60eac3f84d22a1155c3fd10d39
Jonas Pessoa <Jonaspsjw@hotmail.com>
2023-11-23 06:09:24 -0300

    Atualizando App js

---
+++ praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
@@ -0,0 +1,8 @@
+const quizForm = document.querySelector('.quiz-form')
+
+const correctAlternatives = ['D', 'A', 'C', 'B']
+
+quizForm.addEventListener('submit', event => {
+    event.preventDefault()
+    
+})
