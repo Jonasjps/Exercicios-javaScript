a05f89e525875a5c5beb49c289e62ec1f0041b85
Jonas Pessoa <Jonaspsjw@hotmail.com>
2023-11-23 12:32:20 -0300

    Atualizando Refactoring app js

--- praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
+++ praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
@@ -3,24 +3,30 @@ const finalResult = document.querySelector('.result')
 
 const correctAlternatives = ['D', 'A', 'C', 'B']
 
-quizForm.addEventListener('submit', event => {
-    event.preventDefault()
-
-    let score = 0
-    // obetendo respota de usuario
+const getUserAnswers = () => {
     const userAnswers = [
         quizForm.inputQuestion1.value,
         quizForm.inputQuestion2.value,
         quizForm.inputQuestion3.value,
         quizForm.inputQuestion4.value,
     ]
+    return userAnswers
+}
+
+quizForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    let score = 0
+    // obetendo respota de usuario
+    const userAnswers = getUserAnswers()
+    
     //Calculando resposta do usuario
     userAnswers.forEach((userAnswer, index)=> {
         if(userAnswer === correctAlternatives[index]) {
             score += 25
         }
     })
-    //deixando visivel resposta do usuario
+    //deixando visivel a resposta do usuario
     scrollTo(0,0)
 
     finalResult.classList.remove('d-none')
