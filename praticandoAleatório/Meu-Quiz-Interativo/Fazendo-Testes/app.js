d7264668f303adeac349e5261aedab0e7966e5d0
Jonas Pessoa <Jonaspsjw@hotmail.com>
2023-11-25 12:23:11 -0300

    Atualizando app js

--- praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
+++ praticandoAleatório/Meu-Quiz-Interativo/Fazendo-Testes/app.js
@@ -1,1 +1,29 @@
+const quizForm = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
+
 const correctAnswers = ['B', 'C', 'B', 'D']
+
+
+quizForm.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    let score = 0
+    
+    const userAnswers = [
+        quizForm.inputQuestion1.value,
+        quizForm.inputQuestion2.value,
+        quizForm.inputQuestion3.value,
+        quizForm.inputQuestion4.value,
+    ]
+
+    userAnswers.forEach((userAnswer, index)=>  {
+        if(userAnswer === correctAnswers[index]) {
+            score += 25
+        }
+    })
+    finalResult.querySelector('span').textContent = `${score}%`
+    finalResult.classList.remove('d-none')
+
+
+    
+})
