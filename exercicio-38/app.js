70064f7ce72f9e43f52da8aac502a6d6e1f7ac98
Jonas Pessoa <Jonaspsjw@hotmail.com>
2023-12-26 13:14:44 -0300

    Atualizando app

--- exercicio-38/app.js
+++ exercicio-38/app.js
@@ -9,11 +9,17 @@
     diferentes para a propriedade color de cada carro;
   - Teste o método getColor do prototype dos carros.
 */
-const color = color => {
-  return {
-    color,
-  }
+const color = (color) => {
+ return {
+  color,
+ }
+ 
 } 
+
+color.prototype.getColor  = function getColor () {
+  return this.color
+}
+
 
 const siena  = color('black')
 const palio = color('Vermelho')
