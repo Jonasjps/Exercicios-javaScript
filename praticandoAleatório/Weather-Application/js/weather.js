b9c8b884813eeb1c633a81cda00810b140720ba3
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-01-14 19:42:19 -0300

    Atualizando app

--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -2,40 +2,40 @@ const APIKey = 'b6S3NQTmjOfmH75Zwy3OMPYe8stAJYNr'
 
 const getUrl = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-const getCityData = async cityName => {
+const fetchData = async url => {
     try{
-        const cityUrl = getUrl(cityName)
-        const response = await fetch(cityUrl)
+        const response = await fetch(url)
         
         if(!response.ok) {
             throw Error('Não foi possivel obter dados da api.')
         }
 
-        const [cityData] = await response.json()
-        return cityData
+        return await response.json()
         
     } catch ({name, mensage}) {
         alert(`${name} : ${mensage}`)
     }
 }
 
-const getCityWeather = async cityName => {
-    try{
-        const {Key, LocalizedName} = await getCityData(cityName)
-        const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&q=${cityName}&language=pt-br` 
-        const response =  await fetch(weatherUrl)
+const getCityWeather = cityName => fetchData(getUrl(cityName))
 
-        if(!response.ok) {
-            throw Error('Não foi possivel obter dados da API.')
-        }
+// const getCityWeather = async cityName => {
+//     try{
+//         const {Key, LocalizedName} = await getCityData(cityName)
+//         const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&q=${cityName}&language=pt-br` 
+//         const response =  await fetch(weatherUrl)
 
-        const [weatherData] = await response.json()
-        return weatherData
+//         if(!response.ok) {
+//             throw Error('Não foi possivel obter dados da API.')
+//         }
+
+//         const [weatherData] = await response.json()
+//         return weatherData
     
-    } catch ({name, mensage}) {
-        alert(`${name} : ${mensage}`)
-    }
-}
+//     } catch ({name, mensage}) {
+//         alert(`${name} : ${mensage}`)
+//     }
+// }
 
 
 getCityWeather('Brasília')
