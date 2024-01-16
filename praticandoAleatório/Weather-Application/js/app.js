31ada4cfab58471530a9fe483276223fc81d0271
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-01-15 21:49:33 -0300

    Atualizando application js

--- praticandoAleatório/Weather-Application/js/app.js
+++ praticandoAleatório/Weather-Application/js/app.js
@@ -4,13 +4,14 @@ const cityWeatherContainers = document.querySelector('[data-js="city-weather"]')
 const cityTemperatureContainers = document.querySelector('[data-js="city-temperature"]')
 const cityCard = document.querySelector('[data-js="cityCard"]')
 const cityImg = document.querySelector('[data-js="time"]')
+const timeIcon = document.querySelector('[data-js="time-icon]')
 
 form.addEventListener('submit', async event => {
     event.preventDefault()
     const inputForm = event.target.city.value.trim()
     const [{Key, LocalizedName}] = await getCityWeather(inputForm)
-    const [{WeatherText, Temperature, IsDayTime}] = await getCityWeatherData(Key) 
-    
+    const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeatherData(Key) 
+
     if(cityCard.classList.contains('d-none')) {
         cityCard.classList.remove('d-none')
     }
@@ -20,13 +21,12 @@ form.addEventListener('submit', async event => {
     }else {
         cityImg.src = './src/night.svg'
     }
-
     
     cityNameContainers.innerHTML = LocalizedName
     cityWeatherContainers.innerHTML = WeatherText
     cityTemperatureContainers.innerHTML = Temperature.Metric.Value
 
-    
+    event.target.reset()
 })
 
 
--- praticandoAleatório/Weather-Application/js/weather.js
+++ praticandoAleatório/Weather-Application/js/weather.js
@@ -3,8 +3,8 @@ const APIKey = 'WNiGAtAArJcUGkFR1YQyUTGF2vZFczLn'
 const getCityUrl = cityName =>
      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-const getCityWeatherUrl = ({Key}) => 
-    `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br` 
+const getCityWeatherUrl = cityKey => 
+    `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br` 
 
 const fetchData = async url => {
     try{
@@ -23,9 +23,6 @@ const fetchData = async url => {
 
 const getCityWeather = cityName => fetchData(getCityUrl(cityName))
 
-const getCityWeatherData = async cityName => {
-    const [cityData] = await getCityWeather(cityName)
-    return  fetchData(getCityWeatherUrl(cityData))
-}
+const getCityWeatherData = cityKey =>  fetchData(getCityWeatherUrl(cityKey))
 
 
