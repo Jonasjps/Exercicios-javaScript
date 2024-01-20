d09a834af476738aca20d5ce4860816dcb9e823c
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-01-19 23:47:00 -0300

    Atualizando

--- exercicio-38/app.js
+++ exercicio-38/app.js
@@ -111,22 +111,43 @@ const getFormattedTime = template => {
     .join(':')
 }
 
+class Clock {
+  constructor ({ template }) {
+    this.template = template
+  }
 
-// class ExtendedClock extends Clock {
-//   constructor (options) {
-//     super(options)
+  render () {
+    const formattedTime = getFormattedTime(this.template)
+    console.log(formattedTime)
+  }
+
+  start () {
+    const oneSecond = 1000
+
+    this.render()
+    this.timer = setInterval(() => this.render(), oneSecond)
+  }
+
+  stop () {
+    clearInterval(this.timer)
+  }
+}
+
+class ExtendedClock extends Clock {
+  constructor (options) {
+    super(options)
     
-//     const { precision = 1000 } = options
-//     this.precision = precision
-//   }
+    const { precision = 1000 } = options
+    this.precision = precision
+  }
 
-//   start () {
-//     this.render()
-//     this.timer = setInterval(() => this.render(), this.precision)
-//   }
-// }
+  start () {
+    this.render()
+    this.timer = setInterval(() => this.render(), this.precision)
+  }
+}
 
-// const clock = new ExtendedClock({ template: 'h:m:s', precision: 1000 })
+const clock = new ExtendedClock({ template: 'h:m:s', precision: 1000 })
 
 // clock.start()
 
