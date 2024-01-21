06a926d9c3639fe0c246e089d8f65de550061c4f
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-01-20 23:09:58 -0300

    Atualizando

--- exercicio-38/app.js
+++ exercicio-38/app.js
@@ -110,44 +110,66 @@ const getFormattedTime = template => {
     .map((_, index) => formattedTime[index])
     .join(':')
 }
-
-class Clock {
-  constructor ({ template }) {
-    this.template = template
-  }
-
+const makeClock = ({template}) => ({
+  template, 
   render () {
     const formattedTime = getFormattedTime(this.template)
     console.log(formattedTime)
-  }
+  },
 
   start () {
     const oneSecond = 1000
 
     this.render()
     this.timer = setInterval(() => this.render(), oneSecond)
-  }
+  },
 
   stop () {
     clearInterval(this.timer)
   }
-}
+})
 
-class ExtendedClock extends Clock {
-  constructor (options) {
-    super(options)
+const clock = makeClock({template: 'h:m:s', oneSecond: 1000})
+clock.start()
+clock.stop()
+
+// class Clock {
+//   constructor ({ template }) {
+//     this.template = template
+//   }
+
+//   render () {
+//     const formattedTime = getFormattedTime(this.template)
+//     console.log(formattedTime)
+//   }
+
+//   start () {
+//     const oneSecond = 1000
+
+//     this.render()
+//     this.timer = setInterval(() => this.render(), oneSecond)
+//   }
+
+//   stop () {
+//     clearInterval(this.timer)
+//   }
+// }
+
+// class ExtendedClock extends Clock {
+//   constructor (options) {
+//     super(options)
     
-    const { precision = 1000 } = options
-    this.precision = precision
-  }
+//     const { precision = 1000 } = options
+//     this.precision = precision
+//   }
 
-  start () {
-    this.render()
-    this.timer = setInterval(() => this.render(), this.precision)
-  }
-}
+//   start () {
+//     this.render()
+//     this.timer = setInterval(() => this.render(), this.precision)
+//   }
+// }
 
-const clock = new ExtendedClock({ template: 'h:m:s', precision: 1000 })
+// const clock = new ExtendedClock({ template: 'h:m:s', precision: 1000 })
 
 // clock.start()
 
