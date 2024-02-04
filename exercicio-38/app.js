4e3e6ee4783a85b00b47bc4843453c008e9dec41
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-02-04 20:56:52 -0300

    Atualizando Application js

--- exercicio-38/app.js
+++ exercicio-38/app.js
@@ -312,8 +312,13 @@ const exportTable =  () => {
  const currencyOneEl = document.querySelector('[data-js="currency-one"]')
  const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
  const  currencyContainerEl = document.querySelector('[data-js="currency-container"]')
+ const convertedValueEl = document.querySelector('[data-js="converted-value"]')
+ const valuePrecisionEl = document.querySelector('[data-js="conversion-precision"]')
+ const timesCurrencyOneEl = document.querySelector('[data-js="currency-one-times"]')
 
- const getCurrencyUrl = currency => 
+ let internalExchangeRates = {}
+
+ const getUrl = currency => 
   `https://v6.exchangerate-api.com/v6/04cf6b5908dbe464ff892035/latest/${currency}`
   
 const messageError = errorType => ({
@@ -324,9 +329,9 @@ const messageError = errorType => ({
   'quota-reached': 'Sua conta alcançou o limite de requests permitidos em seu plano atual.' 
 })[errorType] || 'Não foi obter dados da moeda informada.'
 
-const getExchangeRates = async () => { 
+const fetchExchangeRates = async (url) => { 
   try{ 
-    const response = await fetch(getCurrencyUrl('KKK'))
+    const response = await fetch(url)
 
     if(!response.ok) {
       throw new Error('Sua conexão falhou. Não foi possível obter as informações.')
@@ -338,7 +343,7 @@ const getExchangeRates = async () => {
       throw new Error(messageError(conversionRates['error-type']))
     }
 
-    console.log(conversionRates)
+    return conversionRates
   }catch (err) {
     
     const div = document.createElement('div')
@@ -359,17 +364,46 @@ const getExchangeRates = async () => {
     div.appendChild(button)
 
     currencyContainerEl.insertAdjacentElement('afterend', div)
-    // console.log(div)
 
   }
+  
 }
 
-getExchangeRates()
+const init = async () => {
 
- const option = `<option>oi</option>`
+  internalExchangeRates = {...(await fetchExchangeRates(getUrl('USD')))}
 
-currencyOneEl.innerHTML = option
-currencyTwoEl.innerHTML = option
+  const getOptions = currencySelect => Object.keys(internalExchangeRates.conversion_rates)
+    .map(currency => `<option ${currency === currencySelect ? 'selected' : ''}>${currency}</option>`) 
+    .join('')
+
+    currencyOneEl.innerHTML = getOptions('USD')
+    currencyTwoEl.innerHTML = getOptions('BRL')
+
+    convertedValueEl.textContent = internalExchangeRates.conversion_rates.BRL.toFixed(2)
+    valuePrecisionEl.textContent = `1 USD = ${internalExchangeRates.conversion_rates.BRL} BRL`
+    
+}
+
+timesCurrencyOneEl.addEventListener('input', e => {
+  convertedValueEl.textContent = (e.target.value * internalExchangeRates.conversion_rates[currencyTwoEl.value]).toFixed(2)
+})
+
+currencyTwoEl.addEventListener('input', e => {
+  const currencyTwoValue = internalExchangeRates.conversion_rates[e.target.value]
+  convertedValueEl.textContent = (timesCurrencyOneEl.value * currencyTwoValue).toFixed(2)
+  valuePrecisionEl.textContent = `1 ${currencyOneEl.value} = ${internalExchangeRates.conversion_rates[currencyTwoEl.value]} ${currencyTwoEl.value}`
+})
+
+currencyOneEl.addEventListener('input', async e => {
+  internalExchangeRates = {...(await fetchExchangeRates(getUrl(e.target.value)))}
+
+  convertedValueEl.textContent = (timesCurrencyOneEl.value * internalExchangeRates.conversion_rates[currencyTwoEl.value]).toFixed(2)
+  valuePrecisionEl.textContent = `1 ${currencyOneEl.value} = ${1 * internalExchangeRates.conversion_rates[currencyTwoEl.value]} ${currencyTwoEl.value}`
+})
+init()
+
+
 
 
 
