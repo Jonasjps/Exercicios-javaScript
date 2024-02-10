6607c2a52044c69608a17b9bf4ae08b94a8c0eb9
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-02-10 16:01:32 -0300

    Atualizando application js

--- exercicio-38/app.js
+++ exercicio-38/app.js
@@ -311,4 +311,53 @@ const exportTable =  () => {
 */
 
 
+const currencyOneEl = document.querySelector('[data-js="currency-one"]')
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
+const currencyContainer = document.querySelector('[data-js=" currency-container"]')
 
+const url = 'https://v6.exchangerate-api.com/v6/04cf6b5908dbe464ff892035/latest/KKK'
+const messageError = typeError => ({
+  'unsupported-code': 'A moeda não existe em nosso banco de dados.',
+  'malformed-request': 'O endpoint do seu resquest precisa seguir a estrutura a seguir: https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD ',
+  'invalid-key': 'A chave da api não é válida.',
+  'inaction-account': 'O seu endereço de email não foi confirmado',
+  'quota-reached': 'Sua conta alcançou o limite de requests permitidos em seu plano atual.' 
+  })[typeError]
+
+const fetchExchangeRates = async () => {
+  try{
+    const response = await fetch(url)
+
+    if(!response.ok){
+      throw new Error('Sua conexão falhou. Não foi possível obter informações .')
+    }
+
+    const exchangeRatesData = await response.json()
+
+    if(exchangeRatesData.result === 'error') {
+      throw new Error(messageError(exchangeRatesData['error-type']))
+    }
+     console.log(exchangeRatesData)    
+  }catch (err) {
+
+    const div = document.createElement('div')
+    const button = document.createElement('button')
+
+    div.textContent = err.message 
+    div.appendChild(button)
+    div.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show')
+    div.setAttribute('role', 'alert')
+    button.setAttribute('aria-label', 'close')
+    button.classList.add('btn-close')
+    currencyContainer.insertAdjacentElement('afterend', div)
+    console.log(div)
+
+  }
+
+} 
+
+fetchExchangeRates()
+const option = `<option>oi</option>`
+
+currencyOneEl.innerHTML = option
+currencyTwoEl.innerHTML = option
