0496c61536f8e0a25f8bc33a729b09519a9427a3
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-06-18 08:02:28 -0300

    Atualizando aula js

--- praticandoAleatório/Firebase/app.js
+++ praticandoAleatório/Firebase/app.js
@@ -1,16 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js'
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiJMFwrQkeNC6Iw8sDbnpJFP3WNAg8-6Q",
-  authDomain: "testing-firebase-a67fc.firebaseapp.com",
-  projectId: "testing-firebase-a67fc",
-  storageBucket: "testing-firebase-a67fc.appspot.com",
-  messagingSenderId: "954304361725",
-  appId: "1:954304361725:web:770e66c05543e4fb223a94",
-  measurementId: "G-T3VHVYGEGZ"
+  apiKey: 'AIzaSyCiJMFwrQkeNC6Iw8sDbnpJFP3WNAg8-6Q',
+  authDomain: 'testing-firebase-a67fc.firebaseapp.com',
+  projectId: 'testing-firebase-a67fc',
+  storageBucket: 'testing-firebase-a67fc.appspot.com',
+  messagingSenderId: '954304361725',
+  appId: '1:954304361725:web:770e66c05543e4fb223a94',
+  measurementId: 'G-T3VHVYGEGZ'
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app)
+getDocs(collection(db, 'games'))
+    .then(querySnapshot => {
+        querySnapshot.docs.forEach(doc => console.log(doc.data()))
+        // querySnapshot.forEach(doc => console.log(doc.data())) 
+    })
+    .catch(console.log)
+
+
+
+
+
