8a24fefbf59f7b1f3bc14b24b0f23a73f25f17f6
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-06-27 06:25:10 -0300

    Atualizando

--- exercicio-39/app.js
+++ exercicio-39/app.js
@@ -204,7 +204,37 @@ const getIndexesOfCharacter = (string, character) =>
       Se essa condição for verdadeira, faça messageIndex receber o valor que 
       ela já tem + 1 e faça characterIndex receber 0.
 */
+const typing = document.querySelector('[data-js="typing"]')
 
+const messages = ['Sou fluente em JS', 'Construo aplicações web com JS puro.']
+
+let messageIndex = 0
+let characterIndex = 0
+let currentMessage = ''
+let currentCharacters = ''
+
+
+const type = () => {
+  const shouldTypeFirstMessage = messageIndex === messages.length
+  
+  if(shouldTypeFirstMessage){
+    messageIndex = 0
+  }
+
+  currentMessage = messages[messageIndex]
+  currentCharacters = currentMessage.slice(0, characterIndex++)
+  typing.textContent = currentCharacters
+
+  const shouldChangeMessageToBeTyped =
+    currentCharacters.length === currentMessage.length
+
+  if( shouldChangeMessageToBeTyped ){
+    messageIndex++
+    characterIndex = 0
+  }
+}
+
+setInterval(type, 200)
 
 
 /*
