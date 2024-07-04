40ba7bd7a948c92bbcafd8907730affb5b22116a
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-07-04 06:26:13 -0300

    Atualizando

--- praticandoAleatório/Firebase/app.js
+++ praticandoAleatório/Firebase/app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js'
-import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js'
+import { getFirestore, collection, getDocs, addDoc, serverTimestamp, doc, deleteDoc, updateDoc } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCiJMFwrQkeNC6Iw8sDbnpJFP3WNAg8-6Q',
@@ -16,12 +16,13 @@ const db = getFirestore(app)
 const collectionGames = collection(db, 'games')
 
 const formAddGame = document.querySelector('[data-js="add-game-form"]')
+const gamesList = document.querySelector('[data-js="games-lis"]')
 
 getDocs(collectionGames)
     .then(querySnapshot => {
-        const gamesLis = querySnapshot.docs.reduce((acc ,doc) => {
+        const gamesLis = querySnapshot.docs.reduce((acc, doc) => {
           const {title, developedBy, createdAt} = doc.data()
-          acc += `<li class="my-4">
+          acc += `<li data-id="${doc.id}" class="my-4">
             <h5>${title}</h5>
 
             <ul>
@@ -29,15 +30,31 @@ getDocs(collectionGames)
               <li>Adicinado no banco em ${createdAt.toDate()}</li>
             </ul>
 
+            <button data-remove="${doc.id}" class="btn btn-danger btn-sm">Remover</button>
+
           </li>`
             
           return acc
         }, '')
-
-        const gamesList = document.querySelector('[data-js="games-lis"]')
+        
         gamesList.innerHTML = gamesLis
       })
     .catch(console.log)
+  gamesList.addEventListener('click', event => {
+    const idRemoveButton = event.target.dataset.remove
+
+    if(idRemoveButton) {
+      
+      deleteDoc(doc(db, 'games',idRemoveButton))
+        .then(() => {
+          const game = document.querySelector(`[data-id="${idRemoveButton}"]`)
+          game.remove()
+          console.log('Game removido')
+        })
+        .catch(console.log)
+    }
+
+  })
 
 
 formAddGame.addEventListener('submit', event => {
@@ -50,8 +67,15 @@ formAddGame.addEventListener('submit', event => {
   })
   .then(doc => console.log('Document criado com ID', doc.id))
   .catch(console.log())
-
-  
 })
+
+const theLastOfUsRef = (doc(db, 'games', '5gUZzTYOvDaC7kMn4URe'))
+
+updateDoc(theLastOfUsRef, {
+  title: 'The Last Of Us Part II',
+  developedBy: 'Valve'
+ })
+  .then(() => console.log('Document atualizado'))
+  .catch(console.log)
 
 
