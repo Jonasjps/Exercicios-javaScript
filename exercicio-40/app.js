da95e3932b5c1e55b8039528b667b1d53c134856
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-07-17 07:37:49 -0300

    Atualizando

--- exercicio-40/app.js
+++ exercicio-40/app.js
@@ -67,12 +67,9 @@ log(removeProp(propToRemove, objs))
   { username: '_rick', status: 'shipped', price: 73.65 }
 ]
 
-const getStatusCount = (orders) => orders.reduce((acc, { status }) => ({
-  ...acc,
-  [status]: (acc[status] || 0) + 1
-}), {})
 
-log(getStatusCount(orders))
+
+// getStatusCount(orders)
 
 /*
 {
