32f8a7fe7e422dd92eebeff91746fa378516b103
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-07-28 21:32:02 -0300

    Atualizando

---
+++ praticandoAleatório/Firebase/firebase-test2/src/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDRhIoZw_J8baJGsn71ezPnH05K8PVXaxg",
+  authDomain: "meuapp-7055c.firebaseapp.com",
+  databaseURL: "https://meuapp-7055c-default-rtdb.firebaseio.com",
+  projectId: "meuapp-7055c",
+  storageBucket: "meuapp-7055c.appspot.com",
+  messagingSenderId: "974131482184",
+  appId: "1:974131482184:web:3db46fcb0ecfb46f20127d",
+  measurementId: "G-B2YNNW5PJS"
+});
+
+
+const auth = getAuth(firebaseApp)
+const db = getFirestore(firebaseApp)
