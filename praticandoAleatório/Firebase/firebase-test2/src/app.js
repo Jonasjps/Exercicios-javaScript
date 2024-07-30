94eb68ee911320fad230897c52fd58116e104fb8
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-07-29 23:01:22 -0300

    Atualizando

--- praticandoAleatório/Firebase/firebase-test2/src/app.js
+++ praticandoAleatório/Firebase/firebase-test2/src/app.js
@@ -1,21 +1,48 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
+// import { initializeApp } from 'firebase/app'
+// import { getDatabase } from 'firebase/database'
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDRhIoZw_J8baJGsn71ezPnH05K8PVXaxg",
-  authDomain: "meuapp-7055c.firebaseapp.com",
-  databaseURL: "https://meuapp-7055c-default-rtdb.firebaseio.com",
-  projectId: "meuapp-7055c",
-  storageBucket: "meuapp-7055c.appspot.com",
-  messagingSenderId: "974131482184",
-  appId: "1:974131482184:web:3db46fcb0ecfb46f20127d",
-  measurementId: "G-B2YNNW5PJS"
-});
+// const firebaseApp = initializeApp({
+//   apiKey: "AIzaSyDRhIoZw_J8baJGsn71ezPnH05K8PVXaxg",
+//   authDomain: "meuapp-7055c.firebaseapp.com",
+//   databaseURL: "https://meuapp-7055c-default-rtdb.firebaseio.com",
+//   projectId: "meuapp-7055c",
+//   storageBucket: "meuapp-7055c.appspot.com",
+//   messagingSenderId: "974131482184",
+//   appId: "1:974131482184:web:3db46fcb0ecfb46f20127d",
+//   measurementId: "G-B2YNNW5PJS"
+// });
+
+// const log = value => console.log(value)
+
+// const database = getDatabase(firebaseApp)
 
 
-const database = getDatabase(firebaseApp)
+// log(database)
+// debugger
 
-const log = value => console.log(value)
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
 
-log(database)
+const firebaseApp = initializeApp({ 
+    apiKey: "AIzaSyDRhIoZw_J8baJGsn71ezPnH05K8PVXaxg",
+    authDomain: "meuapp-7055c.firebaseapp.com",
+    databaseURL: "https://meuapp-7055c-default-rtdb.firebaseio.com",
+    projectId: "meuapp-7055c",
+    storageBucket: "meuapp-7055c.appspot.com",
+    messagingSenderId: "974131482184",
+    appId: "1:974131482184:web:3db46fcb0ecfb46f20127d",
+    measurementId: "G-B2YNNW5PJS"
+ });
+const db = getFirestore(firebaseApp);
+
+async function loadCity(name) {
+  const cityDoc = doc(db, `cities/${name}`);
+  const snapshot = await getDoc(cityDoc);
+  return {
+    id: snapshot.id,
+    ...snapshot.data(),
+  };
+}
+
+loadCity('Brasília')
